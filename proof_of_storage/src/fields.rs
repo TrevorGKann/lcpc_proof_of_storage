ef4836baa6a9660df29838276be681b2cb9aa658
trevor <tkann@cmu.edu>
2023-10-11 14:27:21 -0400

    creating the proof of storage crate too

---
+++ proof_of_storage/src/fields.rs
@@ -0,0 +1,35 @@
+use std::fs::File;
+use std::io::Read;
+use crate::fields::ft253_192::Ft253_192;
+
+pub mod ft253_192 {
+    use ff::PrimeField;
+    use ff_derive_num::Num;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(PrimeField, Num, Deserialize, Serialize)]
+    #[PrimeFieldModulus = "14474011154664524421669271390699307717822958659997404088829842556525106692097"]
+    #[PrimeFieldGenerator = "3"]
+    #[PrimeFieldReprEndianness = "big"]
+    pub struct Ft253_192([u8; 32]);
+}
+
+fn read_file_to_field_elements_vec<F>(path: &str) -> Vec<F>
+where
+    F: ff::PrimeField,
+{
+    let mut file = File::open(path).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+
+    let field_element_width = Ft253_192::size_in_bits() / 8;
+
+    let mut data = Vec::new();
+    for i in 0..(buffer.len() / 32) {
+        let mut tmp = [0u8; 32];
+        tmp.copy_from_slice(&buffer[i * 32..(i + 1) * 32]);
+        let tmp = F::from_random_bytes(&tmp).unwrap();
+        data.push(tmp);
+    }
+    data
+}
---
+++ proof_of_storage/src/main.rs
@@ -0,0 +1,22 @@
+use ff::Field;
+use lcpc_ligero_pc::{LigeroCommit, LigeroEncoding, LigeroEncodingRho};
+use blake3::Hasher as Blake3;
+use typenum::{U1, U2};
+
+mod fields;
+use fields::ft253_192;
+
+fn main() {
+    //Create a compact commit off a matrixLigero encoding with a Rho of 2
+    let mut rng = rand::thread_rng();
+    let data_len = 100;
+    let rows = f64::sqrt(data_len as f64).ceil() as usize;
+    let columns = rows.next_power_of_two();
+    let data: Vec<ft253_192::Ft253_192> = (0..data_len).map(|_| ft253_192::Ft253_192::random(&mut rng)).collect();
+    let encoding = LigeroEncoding::<ft253_192::Ft253_192>::new_from_dims(rows, columns);
+    let commit = LigeroCommit::<Blake3, _>::commit(&data, &encoding).unwrap();
+    // let rho =
+    print!("rho");
+}
+
+
