b3866d65e8d1a50b9901cd34d35090684e852960
Trevor Kann <tkann@andrew.cmu.edu>
2023-10-13 15:18:02 -0400

    working bytes to repr

--- proof_of_storage/src/fields.rs
+++ proof_of_storage/src/fields.rs
@@ -15,23 +15,23 @@ pub mod ft253_192 {
     pub struct Ft253_192([u64; 4]);
 }
 
-fn read_file_to_field_elements_vec<F>(path: &str) -> Vec<F>
-where
-    F: ff::PrimeField,
-{
-    let mut file = File::open(path).unwrap();
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).unwrap();
-
-    let field_element_capacity = ft253_192::Ft253_192::CAPACITY;
-    let field_element_width = ft253_192::Ft253_192::NUM_BITS / 64;
-
-    let mut data = Vec::new();
-    for i in 0..(buffer.len() / 32) {
-        let mut tmp = [0u8; 32];
-        tmp.copy_from_slice(&buffer[i * 32..(i + 1) * 32]);
-        let tmp = F::from_random_bytes(&tmp).unwrap();
-        data.push(tmp);
-    }
-    data
-}
+// fn read_file_to_field_elements_vec<F>(path: &str) -> Vec<F>
+// where
+//     F: ff::PrimeField,
+// {
+//     let mut file = File::open(path).unwrap();
+//     let mut buffer = Vec::new();
+//     file.read_to_end(&mut buffer).unwrap();
+//
+//     let field_element_capacity = ft253_192::Ft253_192::CAPACITY;
+//     let field_element_width = ft253_192::Ft253_192::NUM_BITS / 64;
+//
+//     let mut data = Vec::new();
+//     for i in 0..(buffer.len() / 32) {
+//         let mut tmp = [0u8; 32];
+//         tmp.copy_from_slice(&buffer[i * 32..(i + 1) * 32]);
+//         let tmp = F::from_random_bytes(&tmp).unwrap();
+//         data.push(tmp);
+//     }
+//     data
+// }
--- proof_of_storage/src/main.rs
+++ proof_of_storage/src/main.rs
@@ -1,10 +1,11 @@
-use ff::{Field, PrimeField};
-use lcpc_ligero_pc::{LigeroCommit, LigeroEncoding, LigeroEncodingRho};
 use blake3::Hasher as Blake3;
-use typenum::{U1, U2};
+use ff::{Field, PrimeField};
+use rand::Rng;
+
+use fields::ft253_192;
+use lcpc_ligero_pc::{LigeroCommit, LigeroEncoding};
 
 mod fields;
-use fields::ft253_192;
 
 fn main() {
     //Create a compact commit off a matrixLigero encoding with a Rho of 2
@@ -15,8 +16,16 @@ fn main() {
     let data: Vec<ft253_192::Ft253_192> = (0..data_len).map(|_| ft253_192::Ft253_192::random(&mut rng)).collect();
     let encoding = LigeroEncoding::<ft253_192::Ft253_192>::new_from_dims(rows, columns);
     let commit = LigeroCommit::<Blake3, _>::commit(&data, &encoding).unwrap();
-    // let rho =
-    print!("{}",ft253_192::Ft253_192::NUM_BITS);
+
+    // let bit_width = ft253_192::Ft253_192::CAPACITY;
+    let mut into_values = [0, 60, 42, 59, 158, 247, 242, 112, 246, 228, 145, 198, 106, 73, 57, 183, 65, 94, 171, 55, 9, 162, 142, 33, 195, 17, 64, 232, 152, 8, 242, 249];
+    into_values[0]=0;
+    let my_repr = ft253_192::Ft253_192Repr(into_values);
+    let my_element = ft253_192::Ft253_192::from_repr_vartime(my_repr);
+    println!("{:?}",into_values);
+    println!("{:?}",my_repr);
+    println!("{:?}",my_element);
+    println!("{:?}",ft253_192::Ft253_192::from_str_vartime("106302304611463203484338056704090686885312604975681252400313016821484155641"));
 }
 
 
