4214d7080790140cab7b01f2c0a6043559d4623b
trevor <tkann@cmu.edu>
2023-11-08 14:43:17 -0500

    added new trait for writable field elements.
    new tests require additional interfaces
    will eventually need to write a derive macro for these, but that's in the future

--- proof-of-storage/src/fields.rs
+++ proof-of-storage/src/fields.rs
@@ -4,6 +4,21 @@ use std::io::{Read, Write};
 use std::mem;
 
 use ff::PrimeField;
+use itertools::Itertools;
+use rand::{random, Rng};
+
+pub enum ByteOrder {
+    BigEndian,
+    LittleEndian,
+}
+
+pub trait field_bytes<const U64_WIDTH: usize>: PrimeField {
+    const BYTE_ORDER: ByteOrder;
+
+
+    fn from_u64_array(array: [u64; U64_WIDTH]) -> Option<Self>;
+    fn to_u64_array(&self) -> [u64; U64_WIDTH];
+}
 
 pub mod ft253_192 {
     use ff::PrimeField;
@@ -15,6 +30,40 @@ pub mod ft253_192 {
     #[PrimeFieldGenerator = "3"]
     #[PrimeFieldReprEndianness = "big"]
     pub struct Ft253_192([u64; 4]);
+}
+
+pub mod writable_ft63 {
+    use ff::{Field, PrimeField, FromUniformBytes};
+    use ff_derive_num::Num;
+    use serde::{Deserialize, Serialize};
+    use crate::fields::{ByteOrder, field_bytes};
+
+
+    const U64_WIDTH: usize = 1;
+    const U8_WIDTH: usize = U64_WIDTH * 8;
+
+    #[derive(PrimeField, Num, Deserialize, Serialize)]
+    #[PrimeFieldModulus = "5102708120182849537"]
+    #[PrimeFieldGenerator = "10"]
+    #[PrimeFieldReprEndianness = "little"]
+    pub struct Writeable_Ft63([u64; 1]);
+
+    impl field_bytes<U64_WIDTH> for Writeable_Ft63 {
+        const BYTE_ORDER: ByteOrder = ByteOrder::LittleEndian;
+
+        fn from_u64_array(input: [u64; U64_WIDTH]) -> Option<Self> {
+            let mut ret = Self(input);
+            if ret.is_valid(){
+                return Some(ret);
+            } else {
+                return None;
+            }
+        }
+
+        fn to_u64_array(&self) -> [u64; U64_WIDTH] {
+            self.0
+        }
+    }
 }
 
 pub fn read_file_to_field_elements_vec<F>(path: &str) -> Vec<F>
@@ -47,7 +96,7 @@ where
         .collect()
 }
 
-pub(crate) fn field_elements_vec_to_file<F>(path: &str, field_elements: &[F])
+pub fn field_elements_vec_to_file<F>(path: &str, field_elements: &Vec<F>)
 where
     F: ff::PrimeField,
 {
@@ -60,7 +109,8 @@ where
     for field_element in field_elements {
         let repr = field_element.to_repr();
         let number = repr.as_ref();
-        let write_buffer = &number[write_out_byte_width..];
+        //todo: need to adjust based on BE or LE repr
+        let write_buffer = &number[number.len()-write_out_byte_width..];
 
         //check if we are looking at the last `field_element` in `field_elements`
         //if so, we need to drop trailing zeroes as well
@@ -75,4 +125,26 @@ where
         }
     }
 
+}
+
+pub fn random_writeable_field_vec<F>(log_len: usize) -> Vec<F>
+where
+    F: ff::PrimeField,
+{
+    use std::iter::repeat_with;
+
+    let mut rng = rand::thread_rng();
+
+    let field_element_capacity: usize = F::CAPACITY as usize;
+    let field_element_byte_width: usize = field_element_capacity / 8;
+    let u128_byte_width: usize = mem::size_of::<u128>(); //=16 u8s
+    let read_in_byte_width = min(u128_byte_width, field_element_byte_width);
+    let max_value = 1u128 << (read_in_byte_width*8);
+
+    //create a vector of u8 arrays with len `read_in_byte_width` and fill it with random bytes
+    let random_vector: Vec<F> = repeat_with(|| {
+        let random_value = rng.gen_range(0..max_value);
+        F::from_u128(random_value)
+    }).take(1 << log_len).collect_vec();
+    return random_vector
 }
