ba17a0cc3307a0a5231f6e72bda12c649d86a2e4
trevor <tkann@cmu.edu>
2023-11-09 17:25:14 -0500

    Improved documentation

--- proof-of-storage/src/main.rs
+++ proof-of-storage/src/main.rs
@@ -11,6 +11,7 @@ use lcpc_ligero_pc::{LigeroCommit, LigeroEncoding};
 use lcpc_2d::LcEncoding;
 use proof_of_storage::fields;
 use lcpc_test_fields;
+use lcpc_test_fields::ft63::Ft63;
 
 // type TestField = fields::ft253_192::Ft253_192;
 type TestField = fields::writable_ft63::WriteableFt63;
@@ -22,15 +23,20 @@ fn main() {
     println!("TestField::S = {:?}", <TestField as FieldFFT>::S);
 
     let data: Vec<TestField> = fields::read_file_to_field_elements_vec("proof-of-storage/test_file.txt");
-    let data_width = (data.len() as f32).sqrt().ceil() as usize;
-    let matrix_width = data_width.next_power_of_two();
-    let matrix_colums = (matrix_width + 1).next_power_of_two();
-    let encoding = LigeroEncoding::<TestField>::new_from_dims(matrix_width, matrix_colums);
+    let data_min_width = (data.len() as f32).sqrt().ceil() as usize;
+    let data_realized_width = data_min_width.next_power_of_two();
+    let matrix_colums = (data_realized_width + 1).next_power_of_two();
+    let encoding = LigeroEncoding::<TestField>::new_from_dims(data_realized_width, matrix_colums);
     let commit = LigeroCommit::<Blake3, _>::commit(&data, &encoding).unwrap();
     let root = commit.get_root();
 
+    // randomly select evaluation point of the polynomial
     let x = TestField::random(&mut rand::thread_rng());
 
+    // generate the inner and outer tensor for polynomial evaluation evaluated as
+    // outer_tensor.T * polynomial_matrix * inner_tensor = polynomial(x)
+    // so innter_tensor = [1, x, x^2,...] and outer_tensor = [x^n, x^2n, ...]
+    // where n is the polynomail matrix's number of columns
     let inner_tensor: Vec<TestField> = iterate(TestField::ONE, |&v| v * x)
         .take(commit.get_n_per_row())
         .collect();
@@ -46,8 +52,6 @@ fn main() {
     transcript.append_message(b"ncols", &(encoding.get_n_col_opens() as u64).to_be_bytes()[..]);
 
     let proof = commit.prove(&outer_tensor, &encoding, &mut transcript).unwrap();
-
-    let second_encoding =  LigeroEncoding::<TestField>::new_from_dims(matrix_width, matrix_colums);
     let verification = proof.verify(root.as_ref(), &outer_tensor, &inner_tensor, &encoding, &mut transcript );
 
 
