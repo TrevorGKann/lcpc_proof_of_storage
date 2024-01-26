e6c71f9ecbaef10920f2dfc048c3f82b0dc7e0c1
trevor <tkann@cmu.edu>
2024-01-26 16:28:17 -0500

    Server messages are finally working
    cannot type based on digest at this point for some reason, i think my trait bounds D aren't met for some reason, but it's otherwise working as expected.

--- proof-of-storage/src/networking/server.rs
+++ proof-of-storage/src/networking/server.rs
@@ -7,6 +7,69 @@ use lcpc_2d::{LcCommit, LcEncoding};
 use lcpc_ligero_pc::{LigeroCommit, LigeroEncoding};
 use digest::{Digest, FixedOutputReset, Output};
 use tokio::fs::File;
+use crate::networking::shared;
 
 use crate::networking::shared::*;
 
+
+#[derive(Serialize, Deserialize, Debug)]
+struct MyMessage {
+    field: String,
+}
+#[tokio::test]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let subscriber = tracing_subscriber::fmt()
+        .compact()
+        .with_file(false)
+        .with_line_number(false)
+        .finish();
+    // use that subscriber to process traces emitted after this point
+    tracing::subscriber::set_global_default(subscriber)?;
+
+
+    let listener = TcpListener::bind("0.0.0.0:8080").await
+        .map_err(|e| tracing::error!("server failed to start: {:?}", e))
+        .expect("failed to initialize listener");
+
+    tokio::spawn(async move {
+        //server logic
+
+        while let Ok((stream, _)) = listener.accept().await {
+            tokio::spawn(async move {handle_client_loop(stream).await});
+        }
+    });
+
+
+    // client logic
+    let stream = TcpStream::connect("0.0.0.0:8080").await?;
+    let (mut stream, mut sink) = wrap_stream::<ClientMessages,ServerMessages>(stream);
+
+    sink.send(ClientMessages::UserLogin {username: "trevor".to_owned(), password: "password".to_owned()})
+        .await.expect("Failed to send ping to server");
+
+    let Some(Ok(transmission)) = stream.next().await else {
+        tracing::error!("Failed to receive pong from server");
+        return core::result::Result::Err(Box::from("Failed to receive pong from server"));
+    };
+    tracing::info!("Client received: {:?}", transmission);
+
+    Ok(())
+}
+
+use blake3::Hasher as Blake3;
+use futures::{SinkExt, StreamExt};
+
+async fn handle_client_loop(mut stream: TcpStream) {
+    let (mut stream, mut sink) = wrap_stream::<ServerMessages, ClientMessages>(stream);
+    while let Some(Ok(transmission)) = stream.next().await {
+        tracing::info!("Server received: {:?}", transmission);
+        match transmission {
+            ClientMessages::UserLogin { username, password } => {
+                sink.send(ServerMessages::UserLoginResponse { success: true })
+                    .await
+                    .expect("Failed to send pong");
+            }
+            _ => {}
+        }
+    }
+}
--- proof-of-storage/src/networking/shared.rs
+++ proof-of-storage/src/networking/shared.rs
@@ -1,101 +1,64 @@
-use std::cmp::min;
-use std::time::Duration;
-
 use blake3::Hasher as Blake3;
 use blake3::traits::digest;
+use blake3::traits::digest::FixedOutputReset;
 use digest::Digest;
 use serde::{Deserialize, Serialize};
-use tokio::fs::{File, OpenOptions};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::{TcpListener, TcpStream};
-use tokio::time::timeout;
-use tokio_serde::{Deserializer, Serializer};
-
-use lcpc_2d::{LcCommit, LcEncoding};
-use lcpc_ligero_pc::{LigeroCommit, LigeroEncoding};
-
-use crate::fields::read_file_to_field_elements_vec;
+use tokio::net::{
+    tcp::{OwnedReadHalf, OwnedWriteHalf},
+    TcpStream,
+};
+use tokio_serde::{formats::Json, Framed};
+use lcpc_2d::LcCommit;
+use lcpc_ligero_pc::LigeroEncoding;
+use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
+use tracing::{debug, info, instrument, subscriber};
+use tracing_subscriber;
+use crate::fields;
 use crate::fields::writable_ft63::WriteableFt63;
+
+
 
 type PoSField = WriteableFt63;
 type PoSEncoding = LigeroEncoding<WriteableFt63>;
 type PoSCommit = LcCommit<Blake3, PoSEncoding>;
+type WrappedStream = FramedRead<OwnedReadHalf, LengthDelimitedCodec>;
+type WrappedSink = FramedWrite<OwnedWriteHalf, LengthDelimitedCodec>;
 
-
-async fn start_server(ip: &str, port: &str) -> Result<(), Box<dyn std::error::Error>> {
-    let listening_IP = format!("{}:{}", ip, port);
-    let listener = TcpListener::bind(listening_IP.clone()).await?;
-    println!("Server: Server listening on {listening_IP}");
-
-    while let Ok((stream, _)) = listener.accept().await {
-        tokio::spawn(handle_client_loop(stream));
-    }
-
-    Ok(())
+// We use the unit type in place of the message types since we're
+// only dealing with one half of the IO
+type SerStream<T> = Framed<WrappedStream, T, (), Json<T, ()>>;
+type DeSink<T> = Framed<WrappedSink, (), T, Json<(), T>>;
+pub(crate) fn wrap_stream<Into, OutOf>(stream: TcpStream) -> (SerStream<OutOf>, DeSink<Into>) {
+    let (read, write) = stream.into_split();
+    let stream = WrappedStream::new(read, LengthDelimitedCodec::new());
+    let sink = WrappedSink::new(write, LengthDelimitedCodec::new());
+    (
+        SerStream::new(stream, Json::default()),
+        DeSink::new(sink, Json::default()),
+    )
 }
 
-async fn handle_client_loop(mut stream: TcpStream){
-    println!("Reading line");
-    while let Ok(transmission) = read_transmission(&mut stream).await {
-        println!("Server: Received transmission: {:?}", transmission);
-    }
-
-    println!("Server: Client disconnected");
+type testField = fields::writable_ft63::WriteableFt63;
+#[derive(Debug, Serialize, Deserialize)]
+pub enum ClientMessages {
+    UserLogin{username: String, password: String},
+    UploadNewFile{filename: String, file: Vec<u8>, rows:usize, columns:usize},
+    RequestFile{filename: String},
+    AppendToFile{filename: String, file: Vec<u8>},
+    EditFileRow{filename: String, row: usize, file: Vec<u8>},
+    RequestFileRow{filename: String, row: usize},
+    RequestEncodedRow{filename: String, row: usize},
+    RequestProof{filename: String, columns_to_verify: Vec<usize>},
+    RequestPolynomialEvaluation{filename: String, evaluation_point: testField},
 }
-
-
 
 #[derive(Debug, Serialize, Deserialize)]
-enum Transmission {
-    FileUpload(String),
-    FileDownload(String),
-    // Add other transmission types as needed
-}
-
-
-async fn read_transmission(stream: &mut TcpStream) -> Result<Transmission, Box<dyn std::error::Error>> {
-    let mut buf = Vec::new();
-    stream.read_to_end(&mut buf).await?;
-    println!("transmission read!");
-
-    // Deserialize the content of the buffer into the Transmission enum
-    let transmission: Transmission = serde_json::from_slice(&buf)?;
-    println!("{:?}", transmission);
-
-    Ok(transmission)
-}
-
-async fn send_transmission(stream: &mut TcpStream, transmission: &Transmission) -> Result<(), Box<dyn std::error::Error>> {
-    // Serialize the Transmission enum and send it over the stream
-    let serialized_transmission = serde_json::to_string(transmission)?;
-    stream.write_all(serialized_transmission.as_bytes()).await?;
-
-    Ok(())
-}
-
-#[tokio::test]
-async fn main() {
-    // Start the server
-    tokio::spawn(async {
-        if let Err(e) = start_server("127.0.0.1", "8080").await {
-            eprintln!("Server: Server error: {}", e);
-        }
-    });
-
-    // Wait for the server to start up
-    tokio::time::sleep(Duration::from_secs(1)).await;
-
-    // Start the client
-    let mut client_stream = TcpStream::connect("127.0.0.1:8080").await.unwrap();
-
-    // Example of uploading a file
-    let file_upload = Transmission::FileUpload("uploading_file".to_string());
-    send_transmission(&mut client_stream, &file_upload).await.unwrap();
-
-    // Example of downloading a file
-    let file_download = Transmission::FileDownload("test_file.txt".to_string());
-    send_transmission(&mut client_stream, &file_download).await.unwrap();
-
-    // wait 5 seconds
-    tokio::time::sleep(Duration::from_secs(2)).await;
+// pub enum ServerMessages<D> where D: Digest + FixedOutputReset {
+pub enum ServerMessages{
+    UserLoginResponse{success: bool},
+    CompactCommit{commit: PoSCommit},
+    // MerklePathExpansion{merkle_paths: Vec<Vec<D>>},
+    File{file: Vec<u8>},
+    EncodedRow{row: Vec<testField>},
+    PolynomialEvaluation{evaluation_result: testField},
 }
