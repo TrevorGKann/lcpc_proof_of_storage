6bd286fc4ee1d711532470ce9c546806a8058813
TrevorGKann <tkann@andrew.cmu.edu>
2024-05-01 16:14:40 -0400

    client online commitment verification

--- proof-of-storage/src/lcpc_online.rs
+++ proof-of-storage/src/lcpc_online.rs
@@ -1,6 +1,9 @@
-use blake3::traits::digest::Digest;
+use std::hash::Hash;
 
-use lcpc_2d::{LcEncoding, open_column, ProverError, ProverResult, verify_column_path};
+use blake3::Hasher as Blake3;
+use blake3::traits::digest::{Digest, Output};
+
+use lcpc_2d::{LcEncoding, open_column, VerifierError, VerifierResult, verify_column_path};
 
 use crate::{PoSColumn, PoSCommit, PoSEncoding, PoSField, PoSRoot};
 use crate::file_metadata::ClientOwnedFileMetadata;
@@ -10,7 +13,7 @@ pub type ErrT<E> = <E as LcEncoding>::Err;
 
 
 /// retreive a set of columns from a single commitment to send to a remote client for verification
-pub fn retreive_columns<D, E>(
+pub fn server_retreive_columns<D, E>(
     comm: &PoSCommit,
     requested_columns: &[usize],
 ) -> Vec<PoSColumn>
@@ -26,35 +29,91 @@ pub fn retreive_columns<D, E>(
 }
 
 /// Given a set of columns from a root hash commitment, verify that the supplied columns are valid Merkle paths to data.
-pub fn online_verify_column_paths(
+pub fn client_online_verify_column_paths(
     commitment_root: &PoSRoot,
     requested_columns: &[usize],
     received_columns: &[PoSColumn],
-) -> ProverResult<(), ErrT<PoSEncoding>>
+) -> VerifierResult<(), ErrT<PoSEncoding>>
 {
+    if received_columns.len() != requested_columns.len() {
+        return Err(VerifierError::ColumnEval);
+    }
+
     for (col_num, column) in requested_columns.iter().zip(received_columns.iter()) {
         let path = verify_column_path(column, *col_num, &commitment_root.root);
         if !path {
-            return Err(ProverError::ColumnNumber);
+            return Err(VerifierError::ColumnEval);
         }
     }
-    return Ok(());
+    Ok(())
 }
 
 
 /// Given a set of columns on an initial commitment, verify that the column values match the local encoding of the file
-pub fn online_verify_column_data(
-    locally_derived_columns: &Vec<Vec<PoSField>>,
+pub fn client_online_verify_column_leaves(
+    // optimization: this could either be a random linear combination or just a hash of it
+    locally_derived_column_leaves: &[Output<Blake3>],
     requested_columns: &[usize],
-    received_columns: &Vec<PoSColumn>,
-) -> ProverResult<(), ErrT<PoSEncoding>>
+    eceived_column_leaves: &[Output<Blake3>],
+) -> VerifierResult<(), ErrT<PoSEncoding>>
 {
-    todo!("online_verify_column_data not yet implemented")
+    if locally_derived_column_leaves.len() != requested_columns.len() || eceived_column_leaves.len() != requested_columns.len() {
+        return Err(VerifierError::NumColOpens);
+    }
+
+    let leaves_ok = locally_derived_column_leaves.iter()
+        .zip(eceived_column_leaves.iter())// double iterator through locally derived column leaves and server column leaves
+        .all(|(client_leaf, server_leaf)| client_leaf == server_leaf);
+    //check that all of them agree
+
+    if !leaves_ok {
+        return Err(VerifierError::NumColOpens); //todo should have the right errors here but this will have to do for now
+    }
+
+    Ok(())
 }
 
 pub fn get_PoS_soudness_n_cols(
     file_metadata: &ClientOwnedFileMetadata,
 ) -> usize {
-    let den: f64 = ((1f64 + (file_metadata.encoded_columns as f64 / file_metadata.encoded_columns as f64)) / 2f64).log2();
-    (-128f64 / den).ceil() as usize
+    let denominator: f64 = ((1f64 + (file_metadata.columns as f64 / file_metadata.encoded_columns as f64)) / 2f64).log2();
+    (-128f64 / denominator).ceil() as usize
+}
+
+pub fn client_verify_commitment(
+    commitment_root: &PoSRoot,
+    locally_derived_column_leaves: &[Output<Blake3>],
+    requested_columns: &[usize],
+    received_columns: &[PoSColumn],
+    required_columns_for_soundness: usize,
+) -> VerifierResult<(), ErrT<PoSEncoding>>
+{
+    if required_columns_for_soundness < locally_derived_column_leaves.len()
+        || required_columns_for_soundness < requested_columns.len()
+        || required_columns_for_soundness < received_columns.len()
+    {
+        return Err(VerifierError::NumColOpens);
+    }
+
+    // create a vec of just the received leaves
+    let received_columns_leaves: Vec<Output<Blake3>> = received_columns.iter()
+        .map(|column| column.path[0])
+        .collect();
+
+    client_online_verify_column_leaves(locally_derived_column_leaves, requested_columns, &received_columns_leaves)?;
+    client_online_verify_column_paths(commitment_root, requested_columns, received_columns)?;
+
+    Ok(())
+}
+
+pub fn client_verify_polynomial_evaluation(
+    commitment_root: &PoSRoot,
+    left_evaluation_column: &[PoSField],
+    right_evaluation_column: &[PoSField],
+    evaluation_result: &PoSField,
+    requested_columns: &[usize],
+    received_columns: &[PoSColumn],
+    required_columns_for_soundness: usize,
+) -> VerifierResult<(), ErrT<PoSEncoding>> {
+    todo!();
 }
