f7fb9aaa07874366362f8d0cb98cefcbf35cec57
TrevorGKann <tkann@andrew.cmu.edu>
2025-01-13 14:13:12 -0500

    Adding a new random byte iterator, mainly for running benches.

---
+++ proof-of-storage/src/fields/random_byte_iterator.rs
@@ -0,0 +1,44 @@
+use rand::rngs::ThreadRng;
+use rand_core::RngCore;
+
+pub struct RandomBytesIterator {
+    byte_buffer: [u8; 8192],
+    position: usize,
+    rng: ThreadRng,
+}
+
+impl RandomBytesIterator {
+    pub fn new() -> Self {
+        Self {
+            byte_buffer: [0; 8192],
+            position: 0,
+            rng: rand::thread_rng(),
+        }
+    }
+}
+
+impl Iterator for RandomBytesIterator {
+    type Item = u8;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.position >= self.byte_buffer.len() {
+            self.position = 0;
+        }
+
+        if self.position == 0 {
+            self.rng.fill_bytes(&mut self.byte_buffer);
+        }
+
+        self.position += 1;
+        Some(self.byte_buffer[self.position - 1])
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn can_i_use_take_a_billion() {
+        let mut iterator = RandomBytesIterator::new();
+        iterator.take(usize::max_value());
+    }
+}
