a3afc3c41ce468c4a1102c5900b30e1b7525c3fa
TrevorGKann <tkann@andrew.cmu.edu>
2025-01-13 14:17:34 -0500

    totally overhauled bench to now test if it's bottlenecked by harddrive usage (it's not, at least on my NVME)

--- proof-of-storage/benches/file_to_commit_root.rs
+++ proof-of-storage/benches/file_to_commit_root.rs
@@ -1,30 +1,59 @@
 use blake3::Hasher as Blake3;
-use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion, Throughput};
-use proof_of_storage::fields;
-use proof_of_storage::fields::{is_power_of_two, WriteableFt63};
-use proof_of_storage::lcpc_online::{
-    convert_file_data_to_commit, CommitDimensions, CommitRequestType,
+use criterion::{
+    criterion_group, criterion_main, AxisScale, BenchmarkId, Criterion, PlotConfiguration,
+    Throughput,
 };
+use proof_of_storage::fields::field_generator_iter::FieldGeneratorIter;
+use proof_of_storage::fields::{is_power_of_two, RandomBytesIterator, WriteableFt63};
+use proof_of_storage::lcpc_online::row_generator_iter::RowGeneratorIter;
+use std::io::Read;
 
-fn file_to_commit_root(c: &mut Criterion) {
+fn stream_commit_root(c: &mut Criterion) {
     // let total_size: Vec<u64> = (3..=6).map(|order| pow(10, order)).collect();
     let files = std::fs::read_dir("test_files/").unwrap();
-    let mut group = c.benchmark_group("make_commit");
+
+    let mut group = c.benchmark_group("stream_commit_root");
+    let plot_option = PlotConfiguration::default().summary_scale(AxisScale::Logarithmic);
+    group.plot_config(plot_option);
+
     for file in files {
         let file_path = file.unwrap().path();
+        println!("commiting to \"{}\"", file_path.display());
+        let file_size = std::fs::metadata(&file_path).unwrap().len();
+        let (pre_encoded, encoded) = get_default_dims(file_size as usize);
 
-        let encoded_file_data = fields::random_writeable_field_vec::<WriteableFt63>(logsize);
-        let size = 1 << logsize;
-        group.throughput(Throughput::Elements(size));
-        group.bench_with_input(BenchmarkId::from_parameter(size), &size, |b, &size| {
-            b.iter(|| {
-                let _commit = convert_file_data_to_commit::<Blake3, _>(
-                    &encoded_file_data,
-                    CommitRequestType::Commit,
-                    CommitDimensions::Square,
-                );
-            })
-        });
+        group.throughput(Throughput::Bytes(file_size));
+
+        group.bench_with_input(
+            BenchmarkId::new("streaming from disk", file_size),
+            &file_size,
+            |b, &size| {
+                b.iter(|| {
+                    let buf_reader =
+                        std::io::BufReader::new(std::fs::File::open(&file_path).unwrap())
+                            .bytes()
+                            .map(|b| b.unwrap());
+                    let field_iterator = FieldGeneratorIter::<_, WriteableFt63>::new(buf_reader);
+                    let row_iterator =
+                        RowGeneratorIter::new_ligero(field_iterator, pre_encoded, encoded);
+                    let _streamed_root = row_iterator.convert_to_commit_root::<Blake3>().unwrap();
+                })
+            },
+        );
+
+        group.bench_with_input(
+            BenchmarkId::new("random bytes (no disk)", file_size),
+            &file_size,
+            |b, &size| {
+                b.iter(|| {
+                    let byte_iterator = RandomBytesIterator::new().take(file_size as usize);
+                    let field_iterator = FieldGeneratorIter::<_, WriteableFt63>::new(byte_iterator);
+                    let row_iterator =
+                        RowGeneratorIter::new_ligero(field_iterator, pre_encoded, encoded);
+                    let _streamed_root = row_iterator.convert_to_commit_root::<Blake3>().unwrap();
+                })
+            },
+        );
     }
 }
 
@@ -39,5 +68,5 @@ fn get_default_dims(len: usize) -> (usize, usize) {
     (num_pre_encoded_columns, num_encoded_matrix_columns)
 }
 
-criterion_group!(benches, file_to_commit_root);
+criterion_group!(benches, stream_commit_root);
 criterion_main!(benches);
