47702a9ff0738e322bbb187875c9c2ef06c5a0ff
TrevorGKann <tkann@andrew.cmu.edu>
2025-02-14 15:42:35 -0500

    cleaning up benches

---
+++ proof-of-storage/benches/bench_utils.rs
@@ -0,0 +1,37 @@
+use std::fmt::Display;
+use std::path::PathBuf;
+use tokio::runtime::{Builder, Runtime};
+use tracing_subscriber::EnvFilter;
+
+pub fn start_bench_subscriber() {
+    let env_filter: EnvFilter = EnvFilter::builder()
+        .parse("surrealdb_core=warn,surrealdb=warn,trace")
+        .unwrap();
+
+    let subscriber = tracing_subscriber::fmt()
+        .pretty()
+        .compact()
+        .with_file(true)
+        .with_line_number(true)
+        .with_max_level(tracing::Level::TRACE)
+        .with_env_filter(env_filter)
+        // .with_span_events(FmtSpan::FULL)
+        .finish();
+
+    // use that subscriber to process traces emitted after this point
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+}
+
+pub fn get_bench_runtime() -> Runtime {
+    Builder::new_multi_thread()
+        .enable_all()
+        .worker_threads(8)
+        .build()
+        .expect("couldn't create asynchronous runtime")
+}
+
+pub fn get_bench_single_use_subdir(name: &impl Display) -> PathBuf {
+    let test_dir = PathBuf::from(format!("bench_files/{name}"));
+    std::fs::create_dir_all(&test_dir).expect("couldn't create test directory");
+    test_dir
+}
--- proof-of-storage/benches/edits_on_different_shapes.rs
+++ proof-of-storage/benches/edits_on_different_shapes.rs
@@ -9,145 +9,76 @@ use lcpc_ligero_pc::LigeroEncoding;
 use proof_of_storage::databases::constants;
 use proof_of_storage::fields::WriteableFt63;
 use proof_of_storage::lcpc_online::column_digest_accumulator::ColumnsToCareAbout;
-use proof_of_storage::lcpc_online::encoded_file_writer::EncodedFileWriter;
 use proof_of_storage::lcpc_online::file_handler::FileHandler;
 use proof_of_storage::lcpc_online::{_get_POS_soundness_n_cols, client_online_verify_column_paths};
 use proof_of_storage::networking::client::get_column_indicies_from_random_seed;
 use rand::Rng;
 use rand_chacha::ChaChaRng;
 use rand_core::{RngCore, SeedableRng};
-use std::env;
 use std::path::PathBuf;
-use tokio::fs::OpenOptions;
-use tokio::runtime::{Builder, Runtime};
-use tracing_subscriber::EnvFilter;
+use std::rc::Rc;
+use tokio::runtime::Runtime;
 use ulid::Ulid;
 
+mod bench_utils;
 mod flamegraph_profiler;
 
 fn different_shape_benchmark_main(c: &mut Criterion) {
-    let env_filter: EnvFilter = EnvFilter::builder()
-        .parse("surrealdb_core=warn,surrealdb=warn,trace")
-        .unwrap();
+    bench_utils::start_bench_subscriber();
 
-    let subscriber = tracing_subscriber::fmt()
-        .pretty()
-        .compact()
-        .with_file(true)
-        .with_line_number(true)
-        .with_max_level(tracing::Level::TRACE)
-        .with_env_filter(env_filter)
-        // .with_span_events(FmtSpan::FULL)
-        .finish();
-
-    // use that subscriber to process traces emitted after this point
-    tracing::subscriber::set_global_default(subscriber).unwrap();
-
-    let mut rt = Builder::new_multi_thread()
-        .enable_all()
-        .worker_threads(8)
-        .build()
-        .expect("couldn't create asynchronous runtime");
+    let mut rt = bench_utils::get_bench_runtime();
 
     let test_ulid = Ulid::new();
-    println!(
-        "current directory is {}",
-        env::current_dir().unwrap().display()
-    );
-    let mut test_file_path = env::current_dir().unwrap();
-    test_file_path.push("test_files/1000000000_byte_file.bytes");
-    // test_file_path.push("test_files/10000_byte_file.bytes");
+    let test_file_path = PathBuf::from("test_files/1000000000_byte_file.bytes");
+    // et test_file_path = PathBuf::from(("test_files/10000_byte_file.bytes");
 
-    let test_dir = PathBuf::from(format!("bench_files/{}_test", test_ulid.to_string()));
-    std::fs::create_dir_all(&test_dir).expect("couldn't create test directory");
-    println!("all directories created");
-    if !test_dir.is_dir() {
-        panic!("could not create test directory??");
-    }
+    let test_dir = bench_utils::get_bench_single_use_subdir(&test_ulid.to_string());
     println!("test directory is {}", test_dir.display());
 
-    let mut unencoded_test_file = test_dir.clone();
-    unencoded_test_file.push(format!(
+    let unencoded_test_file = test_dir.join(format!(
         "{}.{}",
         test_ulid.to_string(),
         constants::UNENCODED_FILE_EXTENSION
     ));
-    let mut encoded_test_file = test_dir.clone();
-    encoded_test_file.push(format!(
+    let encoded_test_file = test_dir.join(format!(
         "{}.{}",
         test_ulid.to_string(),
         constants::ENCODED_FILE_EXTENSION
     ));
-    let mut merkle_test_file = test_dir.clone();
-    merkle_test_file.push(format!(
+    let merkle_test_file = test_dir.join(format!(
         "{}.{}",
         test_ulid.to_string(),
         constants::MERKLE_FILE_EXTENSION
     ));
 
-    let powers_of_two_for_pre_encoded_columns: Vec<usize> = (10..17).collect();
-    let mut pre_encoded_len =
-        2usize.pow(*powers_of_two_for_pre_encoded_columns.first().unwrap() as u32);
-    let mut encoded_len = (pre_encoded_len + 1).next_power_of_two();
-
-    println!(
-        "Trying to copy {:?} to {:?}",
-        &test_file_path, &unencoded_test_file
-    );
     std::fs::copy(&test_file_path, &unencoded_test_file).expect("couldn't copy test file");
     let total_file_bytes = std::fs::metadata(&unencoded_test_file).unwrap().len();
-    println!("copied file to test dir");
-    rt.block_on(async {
-        println!("opening (creating) merkle file at {}", merkle_test_file.display());
-        let mut opened_merkle_file = OpenOptions::default().read(true).write(true).create(true).open(&merkle_test_file).await.expect("couldn't open merkle file");
-        println!("opening encoded file at {}", encoded_test_file.display());
-        let mut opened_unencoded_file = OpenOptions::default().read(true).write(true).open(&unencoded_test_file).await.expect("couldn't open unencoded file");
 
-        println!("doing initial encoding of file");
-        let start_time = std::time::Instant::now();
-        EncodedFileWriter::<WriteableFt63, Blake3, LigeroEncoding<WriteableFt63>>::convert_unencoded_file(
-            &mut opened_unencoded_file,
-            &encoded_test_file,
-            Some(&mut opened_merkle_file),
-            pre_encoded_len,
-            encoded_len,
-        )
-            .await
-            .expect("couldn't create initial encoded file");
-        println!("Done encoding file, that took {}ms", start_time.elapsed().as_millis());
-    });
+    let powers_of_two_for_pre_encoded_columns: Vec<u32> = (10..17).collect();
 
     let mut group = c.benchmark_group("edit_file");
     for power_of_two in powers_of_two_for_pre_encoded_columns {
-        println!("Working with {} pre-encoded columns", pre_encoded_len);
-        let mut file_handler = rt.block_on(async {
-            let unencoded_file_path = unencoded_test_file.clone();
-            let encoded_file_path = encoded_test_file.clone();
-            let merkle_file_path = merkle_test_file.clone();
-            FileHandler::<Blake3, WriteableFt63, LigeroEncoding<WriteableFt63>>::new_attach_to_existing_files(
+        let pre_encoded_len = 2usize.pow(power_of_two);
+        let encoded_len = 2usize.pow(power_of_two + 1);
+
+        let start_time = std::time::Instant::now();
+        println!("doing initial encoding of file");
+        let mut file_handler: FileHandler<_,_,_> = rt.block_on(async {
+            FileHandler::<Blake3, WriteableFt63, LigeroEncoding<WriteableFt63>>::create_from_unencoded_file(
                 &test_ulid,
-                unencoded_file_path,
-                encoded_file_path,
-                merkle_file_path,
+                Some(&unencoded_test_file),
                 pre_encoded_len,
                 encoded_len,
             )
                 .await
                 .expect("couldn't open the encoded file")
         });
+        println!(
+            "Done encoding file, that took {}ms",
+            start_time.elapsed().as_millis()
+        );
 
-        pre_encoded_len = 2usize.pow(power_of_two as u32);
-        encoded_len = (pre_encoded_len + 1).next_power_of_two();
-        println!("reshaping the file to the new spec now");
-        rt.block_on(async {
-            file_handler
-                .reshape(pre_encoded_len, encoded_len)
-                .await
-                .expect(
-                    format!("couldn't reshape the encoded file to {}", pre_encoded_len).as_str(),
-                )
-        });
-        println!("file has been reshaped, starting benchmarks now...");
+        println!("Working with {} pre-encoded columns", pre_encoded_len);
 
         edit_benchmark(
             &mut rt,
